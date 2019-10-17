e72b69ebb2d2a785802f7ebf76b2d0c129099ab2
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-17 19:05:30 -0400

    Created Formik form, added Validation and Error Messaging

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import { withFormik, Form, Field, ErrorMessage, yupToFormErrors, Formik } from 'formik';
+import * as Yup from 'yup';
 
-function App() {
+
+
+
+function OnBoardApp({values}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Formik
+          onSubmit={(values, tools) => {
+              console.log(values, tools);
+              tools.resetForm();
+          } }
+          initialvalues={{name: '', email:'', password: '', terms:''}}
+          render = {props => {
+              return(
+                  <Form>
+                      <label>Name </label>
+                      <Field name='name' type='text' placeholder='enter name'/>
+                      <ErrorMessage name='name' component='div' className='warning'/>
+                      <label>Email </label>
+                      <Field name='email' value={values.name} type='email' placeholder='enter email address'/>
+                      <ErrorMessage name='email' component='div' className='warning'/>
+                      <label>Password </label>
+                      <Field name='passord' type='text' placeholder='enter password'/>
+                      <ErrorMessage name='password' component='div' className='warning'/>
+                      <label>Please Agree to Terms of Service </label>
+                      <Field name='terms' type='checkbox' placeholder='Agree to terms of service.'/>
+                      <ErrorMessage name='terms' component='div' className='warning'/>
+                      
+                      <input type='submit' />
+                  </Form>
+              )
+          }}
+      />
+  )
+};
 
-export default App;
+const FormikOnBoardApp = withFormik({
+  mapPropsToValues(name, email, password, terms) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      terms: terms || false
+    };
+  },
+ /// THIS IS WHERE YOU DO THE HTTP REQUEST
+ //--- VALIDATION SCHEMA ----//
+ validationSchema: Yup.object().shape({
+   name: Yup.string()
+    .required("Please enter your name"),
+   email: Yup.string("Please enter a valid email address")
+    .required("Email is required"),
+   password: Yup.string("Please enter your password")
+    .min(6)
+    .required("Password is required")
+ }),
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(OnBoardApp)
+
+export default FormikOnBoardApp;
